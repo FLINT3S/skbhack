7dd12e71d88548b6de41eda73334b4cd544c4e27
siailya <sia.ilya.2017@gmail.com>
2022-11-18 22:34:42 +0300

    Триггер Auth

--- skb-auth/services/api/routes/auth.py
+++ skb-auth/services/api/routes/auth.py
@@ -5,7 +5,7 @@ auth_router = APIRouter()
 
 @auth_router.get("/")
 async def auth():
-    return {"login": "processing 534"}
+    return {"login": "processing 123"}
 
 @auth_router.get("/healthcheck")
 async def healthcheck():
