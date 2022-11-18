59227117f834e4a037017433035c1d3c1c35e726
siailya <sia.ilya.2017@gmail.com>
2022-11-18 21:53:46 +0300

    deploy секции для ZeroTime CD

--- skb-auth/services/api/routes/auth.py
+++ skb-auth/services/api/routes/auth.py
@@ -6,3 +6,7 @@ auth_router = APIRouter()
 @auth_router.get("/")
 async def auth():
     return {"login": "processing 534"}
+
+@auth_router.get("/healthcheck")
+async def healthcheck():
+    return {"healthcheck": "success"}
