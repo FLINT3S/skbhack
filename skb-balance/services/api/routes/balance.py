02966b5b446ffedc728cf55647dd5f589c47bcf6
Philipoff <Filippok2002@yandex.ru>
2022-11-20 01:05:26 +0300

    Поправил возвращение моделей обратно в микросервисы

--- skb-balance/services/api/routes/balance.py
+++ skb-balance/services/api/routes/balance.py
@@ -3,8 +3,8 @@ from sqlmodel import Session, select
 from starlette import status
 from starlette.responses import Response, JSONResponse
 
-from database.service import get_session
-from database.models import *
+from ...database.service import get_session
+from ...database.models import *
 from .dtos import *
 
 balance_router = APIRouter()
