10976a4ceb3d8e40993c94051ef0afb79fbd06d6
Anton <texturich@gmail.com>
2022-11-19 11:52:41 +0300

    вынес строку подключения к бд в .env

--- skb-auth/services/database/service.py
+++ skb-auth/services/database/service.py
@@ -1,19 +1,14 @@
+import os
+
 from sqlmodel import Session, SQLModel, create_engine
-
-from .models import Hero
-
-hero_1 = Hero(name="Deadblonde", secret_name="Dive Wilson")
-hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+from dotenv import load_dotenv
 
 
-engine = create_engine("postgresql://skb:skb123456789@db:5432/skb")
+load_dotenv()
+
+engine = create_engine(os.environ["CONNECTION_STRING"])
 SQLModel.metadata.create_all(engine)
 
+
 def test():
-    with Session(engine) as session:
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-        session.commit()
-        print("Heroes added to the database.")
+    pass
--- skb-balance/database/service.py
+++ skb-balance/database/service.py
@@ -3,22 +3,12 @@ import os
 from sqlmodel import Session, SQLModel, create_engine
 from dotenv import load_dotenv
 
-from .models import Hero
 
 load_dotenv()
-
-hero_1 = Hero(name="Deadblonde", secret_name="Dive Wilson")
-hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-
 
 engine = create_engine(os.environ["CONNECTION_STRING"])
 SQLModel.metadata.create_all(engine)
 
+
 def test():
-    with Session(engine) as session:
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-        session.commit()
-        print("Heroes added to the database.")
+    pass
