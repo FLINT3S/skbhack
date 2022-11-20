3677e692b587d4fe4af95f30bee0c033934fd3dc
Anton <texturich@gmail.com>
2022-11-20 07:38:29 +0300

    исправил вычисление суммы на счету в долларах

--- skb-balance/services/api/routes/balance.py
+++ skb-balance/services/api/routes/balance.py
@@ -87,7 +87,7 @@ async def get_account_history(user_id: UUID, session: Session = Depends(get_sess
     response = list(map(lambda a: {
         "id": str(a.id),
         "amount": a.amount,
-        "amountUSD": a.amount * _get_currency_cost(a.currency.ticker, "USD") ** -1,
+        "amountUSD": a.amount * _get_currency_cost(a.currency.ticker, "USD"),
         "currency": {
             "name": a.currency.name,
             "ticker": a.currency.ticker,
