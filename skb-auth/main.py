90baad9e7879e92f1e4d311b29e83b8e60d2bff6
siailya <sia.ilya.2017@gmail.com>
2022-11-20 03:18:33 +0300

    CORS

--- skb-auth/main.py
+++ skb-auth/main.py
@@ -1,8 +1,18 @@
+from starlette.middleware.cors import CORSMiddleware
+
 from services.api.service import APIService
 from services.database.service import init_db
 import uvicorn
 
 api = APIService()
+
+api.app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     init_db()
